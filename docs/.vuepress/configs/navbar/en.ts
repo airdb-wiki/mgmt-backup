78eaa0ab94c52a95f97ebbcf926b34d9efc4d842
deancn <dean@airdb.com>
2023-02-22 12:58:28 +0800

    feat: reorg

---
+++ docs/.vuepress/configs/navbar/en.ts
@@ -0,0 +1,21 @@
+import type { NavbarConfig } from '@vuepress/theme-default'
+import { version } from '../meta'
+
+export const navbarEn: NavbarConfig = [
+  {
+    text: 'Home',
+    link: 'https://airdb.wiki/',
+  },
+  {
+    text: 'OKR',
+    link: '/okr/',
+  },
+  {
+    text: 'Weekly ',
+    link: '/weekly/',
+  },
+  {
+    text: 'Ideas',
+    link: '/idea/',
+  }
+]
---
+++ docs/.vuepress/configs/sidebar/en.ts
@@ -0,0 +1,46 @@
+import type { SidebarConfig } from '@vuepress/theme-default'
+
+export const sidebarEn: SidebarConfig = {
+  '/okr/': [
+    {
+      text: 'OKR 的背景',
+      children: [
+        '/okr/README.md',
+        '/okr/2022.md',
+        '/okr/2023.md',
+      ],
+    },
+    {
+      text: 'OKR 之剑',
+      children: [
+        '/okr/swordman/README.md',
+      ],
+    },
+  ],
+  '/weekly/': [
+    {
+      text: 'Overview',
+      children: [
+        '/weekly/README.md',
+      ]
+    },
+    {
+      text: '2023',
+      children: [
+        '/weekly/2023.md',
+        '/weekly/2023-q1.md',
+      ]
+    },
+    {
+      text: '2022',
+      collapsible: true,
+      children: [
+        '/weekly/2022.md',
+        '/weekly/2022-q1.md',
+        '/weekly/2022-q2.md',
+        '/weekly/2022-q3.md',
+        '/weekly/2022-q4.md',
+      ]
+    }
+  ]
+}
